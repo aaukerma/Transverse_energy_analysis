fe5c848f0460de26e2717dc4bb593d9615bd0154
smithbww <bsmit210@vols.utk.edu>
2018-05-25 11:19:33 -0400

    Program Complete, Pi0 data now in BESData_sorted_PlusPi0.txt. (running program will create it if not there (takes data from BESData_sorted.txt. pi0 data appears after pi+ data

--- BESData_InclPi0.cpp
+++ BESData_InclPi0.cpp
@@ -2,7 +2,7 @@
 /*This program will take data from BESData_sorted.txt and include
 approximated data for Pi0 particle. built from BESDataToRootFile.cpp
 start:20180522.11:00
-compl:--
+compl:20180525.11:14
 */
 
 #include <cstdio>
@@ -26,12 +26,12 @@ struct Bin {
   BinData Dat;
 };
 
-void pi0Builder(const vector<vector<Bin>>& vect1, const vector<vector<Bin>>& vect2);
+void pi0Builder(const vector<vector<Bin>>& vect1, const vector<vector<Bin>>& vect2, vector<vector<Bin>>& Pi0);
 
 int main(){
   string CollType;
   string CollEner;
-  string title0 = "------------------------";
+  string title0 = "------------------------"; //This is manually entered (it could potentially be read in directly)[applies to all "titleX"]
   string title1 = "pTlow [GeV/c]";
   string title2 = "pThigh [GeV/c]";
   string title3 = "d^2N/(2pi*pt*dpt*dy)[(GeV/c)^-2]";
@@ -41,20 +41,23 @@ int main(){
   string PartType;
   string CentBin;
   string GARBAGE;
-  int BinNum = 0;
+  int c1;
+  int c2;
   int j;
   int k;
   int l=0;
+  int PiSize;
   double DataPoint;
   vector <Bin> PiM(0);
   vector <Bin> PiP(0);
   vector <vector <Bin>> PiMinus(9);
   vector <vector <Bin>> PiPlus(9);
+  vector <vector <Bin>> Pi0(9);
 
   ifstream in;
-	in.open("./BESData_sorted.txt");
+	in.open("./BESData_sorted.txt"); //Formatting must match previous versions!!!!
   ofstream out;
-  out.open("./BESData_sorted_PlusPi0.txt");
+  out.open("./BESData_sorted_PlusPi0.txt"); //created file for inclusion of pi0, formatted!
 
   while(in>>CollType){
     while(CollType!="Au+Au") {in>>CollType;}
@@ -66,7 +69,7 @@ int main(){
 
     for(j = 0; j<9; j++) {
       in>>PartType;
-      if (j>=1){
+      if (j>=1){ //this is a quick fix, there is a 'title0' that appears and has to be skipped. doesnt work at end of loop for some reason
         in>>PartType;
       }
       else{}
@@ -94,7 +97,6 @@ int main(){
         }
       }
       else if (PartType =="pi+") { //write in data for vector PiPlus and writes to new file
-        BinNum = 0;
         k=0;
         while (in>>DataPoint){
           PiP.push_back(Bin());
@@ -115,10 +117,7 @@ int main(){
         }
       }
       else {//writes to new file
-        BinNum = 0;
-        k=0;
         while (in>>DataPoint){
-          BinNum++;
           //in>>DataPoint; this is already done in boolean
           out<<fixed<<setprecision(2)<<DataPoint<<" \t ";
           in>>DataPoint;
@@ -135,12 +134,37 @@ int main(){
       in.clear();
     } //end forloop ()
     in>>GARBAGE;
-    pi0Builder(PiMinus, PiPlus);
     out<<title6<<endl;
-    in.clear();
+    if (l==1){
+      pi0Builder(PiMinus, PiPlus, Pi0); //runs once for each energy group after pi+ is fed
+      out<<CollType<<" ";
+      out<<CollEner<<" GeV"<<endl;
+      out <<title0<<endl<<title1<<" "<<title2<<" "<<title3<<" "<<title4<<" "<<title5<<endl;
+      for (int n=0; n<9; n++) { //writes in Pi0
+        c1=n*10;
+        c2=(n+1)*10;
+        out<<"pi0  "<<c1<<"-"<<c2<<'%'<<endl;
+        PiSize=Pi0[n].size();
+        for (int m=0; m<PiSize;m++) {
+          if (Pi0[n][m].Dat.pTl != 0) {
+            out<<fixed<<setprecision(2)<<Pi0[n][m].Dat.pTl<<" \t ";
+            out<<fixed<<setprecision(2)<<Pi0[n][m].Dat.pTh<<" \t ";
+            out<<fixed<<setprecision(4)<<Pi0[n][m].Dat.pTSpec<<" \t ";
+            out<<fixed<<setprecision(5)<<Pi0[n][m].Dat.ErrStat<<" \t ";
+            out<<fixed<<setprecision(5)<<Pi0[n][m].Dat.ErrSys<<endl;
+          }
+        }
+        out<<title0<<endl;
+      }
+      out<<title6<<endl;
+    }
     l++;
+    if (l==6) {l=0;}
+    in.clear();
   } //end while (in>>CollType)
 cout<<"done";
+in.close();
+out.close();
 return 0;
 } //end int 'main'
 
@@ -148,6 +172,48 @@ return 0;
 //pi0Builder passes in vectors consisting of all pi- and pi+ data points.
 //pi0Builder takes average of all points and creates new vector with
 //all data for pi0 then writes it to file.
-void pi0Builder(const vector<vector<Bin>>& vect1, const vector<vector<Bin>>& vect2){
-
+void pi0Builder(const vector<vector<Bin>>& vect1, const vector<vector<Bin>>& vect2, vector<vector<Bin>>& Pi0){
+  //int j=0; //size counter for vect1
+  //int k=0; //size coutner for vect2 (may be different than vect1)
+  int size;
+  Bin pim;
+  Bin pip;
+  Bin pizero;
+  for (int i=0; i<9; i++){
+    /*if (vect1[i][0].Dat.pTl > vect2[i][0].Dat.pTl) {
+      while (vect1[i][0].Dat.pTl != vect2[i][k].Dat.pTl) {
+        k++;
+      }
+    }
+    else if (vect1[i][0].Dat.pTl < vect2[i][0].Dat.pTl) {
+      while (vect1[i][j].Dat.pTl != vect2[i][0].Dat.pTl) {
+        j++;
+      }
+    }
+    else {
+    }*/  //may need if .Dat.pTl do not match for first value for different data set
+    if (vect1[i].size() > vect2[i].size()) {
+      size=vect2[i].size();
+    }
+    else //(vect1[i].size() <= vect2[i].size())
+      size=vect1[i].size();
+    for (int j=0; j<size;j++){
+      pim.Dat.pTSpec=vect1[i][j].Dat.pTSpec;
+      pim.Dat.ErrStat=vect1[i][j].Dat.ErrStat;
+      pim.Dat.ErrSys=vect1[i][j].Dat.ErrSys;
+      pip.Dat.pTSpec=vect2[i][j].Dat.pTSpec;
+      pip.Dat.ErrStat=vect2[i][j].Dat.ErrStat;
+      pip.Dat.ErrSys=vect2[i][j].Dat.ErrSys;
+      pizero.BinIndex=j;
+      pizero.Dat.pTl=vect1[i][j].Dat.pTl;
+      pizero.Dat.pTh=vect1[i][j].Dat.pTh;
+      pizero.Dat.pTSpec= (pim.Dat.pTSpec+pip.Dat.pTSpec)/2;
+      pizero.Dat.ErrStat= (pim.Dat.ErrStat+pip.Dat.ErrStat)/2;
+      pizero.Dat.ErrSys= (pim.Dat.ErrSys+pip.Dat.ErrSys)/2;
+      if (pizero.Dat.pTSpec !=0){
+        Pi0[i].push_back(Bin());
+        Pi0[i][j]=pizero;
+      }
+    }
+  }
 }
