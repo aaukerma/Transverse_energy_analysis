890c36b2040b3011a15387a8788eed2d87bd41c4
smithbww <bsmit210@vols.utk.edu>
2018-06-14 13:26:12 -0400

    made changes to work with aliRoot

--- PYTHIA/driver.C
+++ PYTHIA/driver.C
@@ -1,6 +1,8 @@
 void driver(Int_t nEvents = 100, Int_t jobID = 0, Int_t tune = 100){
-  gSystem->Load("$PYTHIA6/libPythia6"); //change to your setup
+  gSystem->Load("libPythia6"); //change to your setup
   gSystem->Load("libEGPythia6");
+  gSystem->Load("libEG");
+  gSystem->Load("libAliPythia6");
   gROOT->ProcessLine(".L SimplePYTHIALoop.C++");
   makeEventSample(nEvents,jobID,tune);
 
