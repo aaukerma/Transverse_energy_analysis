700e291356458a218e297b624f61a4ed371b0b17
smithbww <bsmit210@vols.utk.edu>
2019-05-20 08:53:41 -0400

    working code (from andy) for APMT

---
+++ AMPT/runtest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+# The settings come from ~/.bash_profile
+
+rm -rf */*.root */*.log */*.dat */GRP */*.ps */AliHLT*
+cd ./gen
+aliroot -b -q rungen.C\(5\) 2>&1 | tee gen.log
+#aliroot -b -q fastMcProduction.C\(5\) 2>&1 | tee gen.log
+chmod a-w *.root
+cd ../sim
+aliroot -b -q sim.C\(5\) 2>&1 | tee sim.log
+aliroot -b -q rec.C      2>&1 | tee rec.log
+aliroot -b -q ${ALICE_ROOT}/STEER/macros/CheckESD.C 2>&1 | tee check.log
+aliroot -b -q aod.C 2>&1 | tee aod.log
+
+
