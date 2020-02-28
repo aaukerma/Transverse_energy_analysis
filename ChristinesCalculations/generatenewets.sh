251bf930b2c60d2afe5744b3adeb2d6a28fa155b
cnattras <saccharomyces.cerevisae@gmail.com>
2020-02-28 14:33:15 -0500

    minor tweaks

--- ChristinesCalculations/generatenewets.sh
+++ ChristinesCalculations/generatenewets.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#Tanner's output is written in Averaged_PiKp_ET_results.txt & Averaged_Lam_ET_resutls.txt
+#It creates
+#-rw-rw-r-- 1 captainq captainq   7628 Feb 28 14:26 EtResults.txt
+#-rw-rw-r-- 1 captainq captainq   7564 Feb 28 14:26 EtResultsN.txt
+#-rw-rw-r-- 1 captainq captainq   7526 Feb 28 14:26 EtResultsnpart.txt
 g++ -o calc CalcFinalETs.cpp
 ./calc
