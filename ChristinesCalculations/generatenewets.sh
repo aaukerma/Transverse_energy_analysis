f608fa7c4c5630d99e468e063df54b6b19793a13
cnattras <saccharomyces.cerevisae@gmail.com>
2020-03-03 07:59:34 -0500

    I think there are a couple changes here

--- ChristinesCalculations/generatenewets.sh
+++ ChristinesCalculations/generatenewets.sh
@@ -3,6 +3,8 @@
 #It creates
 #-rw-rw-r-- 1 captainq captainq   7628 Feb 28 14:26 EtResults.txt
 #-rw-rw-r-- 1 captainq captainq   7564 Feb 28 14:26 EtResultsN.txt
-#-rw-rw-r-- 1 captainq captainq   7526 Feb 28 14:26 EtResultsnpart.txt
+#-rw-rw-r-- 1 captainq captainq   7526 Feb 28 14:26 EtResultsnpart.txt <-- Ben says ignore this it is garbage
+# In the other files "c" and "u" are correlated and uncorrelated assumptions about uncertainties
+
 g++ -o calc CalcFinalETs.cpp
 ./calc
